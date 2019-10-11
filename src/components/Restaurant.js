769105646962e11f382d15c34ec560c05dc0d97b
Chawit.O <chawit.ocr@gmail.com>
2019-10-11 15:29:00 +0100

    restaurantShow page

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -7,7 +7,8 @@ export default class Restaurant extends React.Component {
     super()
 
     this.state = {
-      restData: null
+      restData: null,
+      reviewData: null
     }
     this.headers = { headers: { 'user-key': process.env.ZOMATO_TOKEN } }
   }
@@ -18,49 +19,91 @@ export default class Restaurant extends React.Component {
     axios.get(url, this.headers)
       .then(res => this.setState({ restData: res.data }))
       .catch(err => console.log(err.message))
+
+    const revUrl = `https://developers.zomato.com/api/v2.1/reviews?res_id=${this.props.match.params.id}&start=0&count=10`
+    axios.get(revUrl, this.headers)
+      .then(rev => this.setState({ reviewData: rev.data }))
+      .catch(err => console.log(err.message))
   }
 
   render() {
-    if (!this.state.restData) return null
+    if (!this.state.restData || !this.state.reviewData) return null
     const { thumb, name, featured_image,
       location: { locality, address }, establishment,
       user_rating: { aggregate_rating, rating_text, rating_color },
-      all_reviews_count, phone_numbers, cuisines, average_cost_for_two, currency, url, timings, highlights
+      all_reviews: { reviews },
+      all_reviews_count, phone_numbers, cuisines, average_cost_for_two, currency, url, timings, highlights,
+      menu_url, photos
     } = this.state.restData
+    const { user_reviews } = this.state.reviewData
     return (
       this.state.restData &&
-      <>
-        <div>
-          <img src={ featured_image } />
-          <h1>{name}</h1>
-          <h2>{locality} {establishment[0]}</h2>
-          <aside>
-            {aggregate_rating}
-            {rating_text}
-            {rating_color}
-            based on {all_reviews_count} votes
-          </aside>
+      <main className='restaurant-show'>
+        <img src={featured_image} />
+        <div className='panel-wrapper restDetails'>
+          <div className='restMainInfo'>
+            <h2>{name}</h2>
+            <p>{locality} - {establishment[0]}</p>
+            <div className='rating'>
+              <div style={{ backgroundColor: `#${rating_color}` }}>{aggregate_rating}<span>/5</span></div>
+              <p>{rating_text}</p>
+              <p>{all_reviews_count} votes</p>
+            </div>
+          </div>
+          <div>
+            <h4>Phone number</h4>
+            <p>{phone_numbers}</p>
+            <h4>Cuisines </h4>
+            <p>{cuisines}</p>
+            <h4>Average Cost</h4>
+            <p>{currency}{average_cost_for_two} for two people (approx.)</p>
+            <p>Cash and cards accepted </p>
+            <h4>Website</h4>
+            <a href={url}>View Website</a>
+          </div>
+          <div>
+            <h4>Opening Hours</h4>
+            <p>{timings}</p>
+            <h4>Address</h4>
+            <p>{address}</p>
+          </div>
+          <div>
+            <h4>More info</h4>
+            <h4>highlights</h4>
+            <ul>
+              {highlights.map(hl => <li key={hl}>{hl}</li>)}
+            </ul>
+          </div>
+        </div>
+        <div className='panel-wrapper restDetails'>
+          <div>
+            <h4>Menu</h4>
+            <a href={menu_url}>click for menu</a>
+          </div>
+        </div>
+        <div className='panel-wrapper restPhotos'>
+          <div>
+            <h4>Photos</h4>
+            {photos.map(photo => <img key={photo.photo.id} src={photo.photo.url} />)}
+          </div>
         </div>
         <div>
-          <h4> Phone number {phone_numbers}</h4>
-          <h4> Cuisines {cuisines}</h4>
-          <h4> {currency}{average_cost_for_two} for two people (approx.)</h4>
-          <h4> {average_cost_for_two} for two people (approx.)</h4>
-          <h4><a href={url}>Website</a></h4>
-          <h4>Opening Hours {timings}</h4>
-          <h4>Address {address}</h4>
-          <h4>Address {address}</h4>
-          <h4>More info</h4>
-          <h4>highlights</h4>
-          <ul>
-            {highlights.map(hl => ( <li>hl</li> ))}
-          </ul>
+          <h4>Reviews</h4>
+          {user_reviews.filter(({ review }) => review.review_text.length).map(({ review }) => (
+            <div key={review.id} className='panel-wrapper restReview'>
+              <div className='reviewer-detail'>
+                <img src={review.user.profile_image} />
+                <h5>{review.user.name}</h5>
+                <p>{review.review_time_friendly}</p>
+              </div>
+              <div className='rating'>
+                <div style={{ backgroundColor: `#${review.rating_color}` }}>{review.rating}</div>
+              </div>
+              <p> {review.review_text} </p>
+            </div>
+          ))}
         </div>
-        <div>
-          <h4>Menu</h4>
-          <img />
-        </div>
-      </>
+      </main>
     )
   }
 }
